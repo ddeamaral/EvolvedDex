d950000f8499bac13f5bf96ddb1b56305688adf1
Dylan DeAmaral <deamaral.dylan@gmail.com>
2017-01-29 13:43:01 -0500

    Adding Evolved Dex code

---
+++ Evolved Dex/EvolvedDex/src/EvolvedDex/Models/Contexts/DataContext/PokedexContext.cs
@@ -0,0 +1,23 @@
+﻿using EvolvedDex.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EvolvedDex.Models.Contexts.DataContext
+{
+    public class PokedexContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public PokedexContext(DbContextOptions options, IConfigurationRoot builder) : base(options)
+        {
+            connectionString = builder.GetConnectionString("PokedexDb");
+        }
+        
+        public DbSet<Pokemon> Pokemon { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySql(connectionString);           
+        
+
+    }
+}
---
+++ Evolved Dex/EvolvedDex/src/EvolvedDex/Models/Entities/Pokemon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvolvedDex.Models.Entities
+{
+    public class Pokemon
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Range(1, 801, ErrorMessage = "")]
+        public int PokedexNumber { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double BaseAttack { get; set; }
+
+        [Required]
+        public double BaseDefense { get; set; }
+
+        [Required]
+        public double BaseSpeed { get; set; }
+
+        [Required]
+        public double BaseSpecialAttack { get; set; }
+
+        [Required]
+        public double BaseSpecialDefense { get; set; }
+
+        //[Required]
+        //public List<string> ImagePath { get; set; }
+
+        [StringLength(1000, MinimumLength = 20, ErrorMessage = "Description must be between {1} and {0} characters long.")]
+        public string Description { get; set; }
+
+        [Required]
+        public bool isInMod { get; set; }
+
+        [NotMapped]
+        public string DateReleased { get; set; }
+
+
+    }
+}
